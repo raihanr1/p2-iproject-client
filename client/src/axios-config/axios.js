80eedf8aa5c76cb86b8d573ec72b996bbc9000e4
raihanr1 <raihanrobbani123@gmail.com>
2021-11-17 19:59:48 +0700

    fitur register is done

--- client/src/axios-config/axios.js
+++ client/src/axios-config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://7210-182-253-127-194.ngrok.io",
+  baseURL: "https://7e42-182-253-127-194.ngrok.io",
 });
 
 export default instance;
--- client/src/store/modules/users/mutations.js
+++ client/src/store/modules/users/mutations.js
@@ -1,1 +1,16 @@
-export default {};
+export default {
+  HANDLE_LOGIN_ACTION: function (state, payload) {
+    if (payload.access_token) {
+      localStorage.setItem("access_token", payload.access_token);
+      state.isLogin = true;
+    } else {
+      state.token = payload.token;
+    }
+  },
+  HANDLE_REGISTER_ACTION: function (state, status) {
+    state.isRegister = status;
+  },
+  HANDLE_ERROR_ACTION: function (state, error) {
+    state.error = error;
+  },
+};
